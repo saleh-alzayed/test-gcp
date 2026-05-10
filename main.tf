e44160f0383a971ecfbade81ccd843e204d3734e
saleh-alzayed <s.f.alzayed@gmail.com>
2026-05-11 02:13:14 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = "project-21285119-0207-4055-9b0"
+  region  = "us-central1"
+}
+
+resource "google_compute_network" "main_vpc" {
+  name                    = "main-vpc"
+  auto_create_subnetworks = false
+}
