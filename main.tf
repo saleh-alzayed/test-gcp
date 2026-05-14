5c64b08f57b964bcb0ad4f042e6d5763e47a2216
saleh-alzayed <s.f.alzayed@gmail.com>
2026-05-14 03:14:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  project = "project-14150041-320a-4620-b46"
-  region  = "us-central1"
+  project = "project-14150041-320a-4620-b46"
+  region  = "us-central1"
 }
 
 resource "google_compute_network" "main_vpc" {
-  name                    = "main-vpc1"
-  auto_create_subnetworks = false
+  name                    = "main-vpc1"
+  auto_create_subnetworks = false
 }
